2a9960400b36e41a05f3b16418b00284592c2f88
Tiago Pastorello <tiagopastorello@gmail.com>
2024-07-02 19:29:15 -0300

    Capacitor SIM Info

--- android/src/main/java/com/vinteum/cap/siminfo/SimInfoPlugin.java
+++ android/src/main/java/com/vinteum/cap/siminfo/SimInfoPlugin.java
@@ -1,22 +1,31 @@
-package com.vinteum.cap.siminfo;
+package com.yourdomain.siminfo;
+
+import android.content.Context;
+import android.Manifest;
+import android.telephony.TelephonyManager;
+
+import androidx.annotation.RequiresPermission;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.Permission;
 
-@CapacitorPlugin(name = "SimInfo")
+@CapacitorPlugin(name = "SimInfo", permissions = {@Permission(strings = {Manifest.permission.READ_PHONE_STATE}, alias = "phoneState")})
 public class SimInfoPlugin extends Plugin {
 
-    private SimInfo implementation = new SimInfo();
-
+    @RequiresPermission(Manifest.permission.READ_PHONE_STATE)
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void getSimInfo(PluginCall call) {
+        Context context = getContext();
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        ret.put("countryIso", telephonyManager.getSimCountryIso());
+        ret.put("operator", telephonyManager.getSimOperator());
+        ret.put("operatorName", telephonyManager.getSimOperatorName());
+        ret.put("serialNumber", telephonyManager.getSimSerialNumber());
         call.resolve(ret);
     }
 }
